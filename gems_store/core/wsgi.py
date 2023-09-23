73997d679ae4e62f86357a357187d4c193e75987
a-menshikov <a.menshikov1989@gmail.com>
2023-09-23 16:16:08 +0300

    init project

---
+++ gems_store/core/wsgi.py
@@ -0,0 +1,8 @@
+import os
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = StaticFilesHandler(get_wsgi_application())
