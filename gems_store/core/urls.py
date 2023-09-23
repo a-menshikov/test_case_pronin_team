60ad7b04ee2af252b1dec480a8f3dcde950ab60b
a-menshikov <a.menshikov1989@gmail.com>
2023-09-23 19:56:19 +0300

    api app and url structure

--- gems_store/core/urls.py
+++ gems_store/core/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
+
+admin.site.site_header = 'Store Admin'
+admin.site.site_title = 'gems_store'
+admin.site.index_title = 'Панель администратора'
