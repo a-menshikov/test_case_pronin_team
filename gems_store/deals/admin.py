4d214726f528c11adc16f6045841ebcc63d82cf7
a-menshikov <a.menshikov1989@gmail.com>
2023-09-23 18:18:11 +0300

    models and admin

--- gems_store/deals/admin.py
+++ gems_store/deals/admin.py
@@ -1,1 +1,30 @@
 from django.contrib import admin
+
+from deals.models import Customer, Deal, Item
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Админ-панель модели покупателя."""
+
+    list_display = ('id', 'login')
+    ordering = ('id',)
+    list_per_page = 50
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    """Админ-панель модели товара."""
+
+    list_display = ('id', 'name')
+    ordering = ('id',)
+    list_per_page = 50
+
+
+@admin.register(Deal)
+class DealAdmin(admin.ModelAdmin):
+    """Админ-панель модели сделки."""
+
+    list_display = ('id', 'date', 'customer', 'item', 'total', 'quantity')
+    list_filter = ('customer', 'item')
+    list_per_page = 50
--- gems_store/deals/models.py
+++ gems_store/deals/models.py
@@ -1,1 +1,73 @@
 from django.db import models
+
+
+class Customer(models.Model):
+    """Модель покупателя."""
+
+    login = models.CharField(
+        max_length=128,
+        verbose_name='Логин',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Покупатель'
+        verbose_name_plural = 'Покупатели'
+
+    def __str__(self) -> str:
+        return self.login
+
+
+class Item(models.Model):
+    """Модель товара."""
+
+    name = models.CharField(
+        max_length=128,
+        verbose_name='Название',
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Deal(models.Model):
+    """Модель сделки."""
+
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name='deals',
+        verbose_name='Покупатель',
+    )
+
+    item = models.ForeignKey(
+        Item,
+        on_delete=models.CASCADE,
+        related_name='deals',
+        verbose_name='Товар',
+    )
+
+    total = models.PositiveIntegerField(
+        verbose_name='Сумма',
+    )
+
+    quantity = models.PositiveIntegerField(
+        verbose_name='Количество',
+    )
+
+    date = models.DateTimeField(
+        verbose_name='Дата',
+    )
+
+    class Meta:
+        verbose_name = 'Сделка'
+        verbose_name_plural = 'Сделки'
+        ordering = ['-date']
+
+    def __str__(self) -> str:
+        return f'{self.customer} - {self.item}'
