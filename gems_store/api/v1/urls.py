3fb8b527f8a13ea6a06fedaad8a6e9c2feac7cd0
a-menshikov <a.menshikov1989@gmail.com>
2023-09-25 01:56:55 +0300

    dirty get endporint

--- gems_store/api/v1/urls.py
+++ gems_store/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.v1.views import add_deals
+from api.v1.views import add_deals, get_top_customers
 
 urlpatterns = [
     path('add-deals', add_deals),
+    path('get-top', get_top_customers),
 ]
--- gems_store/api/v1/views.py
+++ gems_store/api/v1/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from api.v1.service import Service
+from deals.models import Customer, Item
+from django.db.models import Sum
 
 service = Service()
 
@@ -25,3 +27,35 @@ def add_deals(request):
                 'Desc': error_message,
             }
         )
+
+
+@api_view(['GET'])
+def get_top_customers(request):
+    """Получение списка покупателей."""
+    top_customers = Customer.objects.annotate(
+        spent_money=Sum('deals__total'),
+    ).order_by('-spent_money')[:5]
+
+    items_per_customer = Item.objects.filter(
+        deals__customer__in=top_customers
+    ).values_list(
+        'deals__customer__login', 'name'
+    ).distinct()
+
+    response = []
+
+    gems_lists = {obj.login: [] for obj in top_customers}
+    gems_count = {}
+
+    for i in items_per_customer:
+        gems_lists[i[0]].append(i[1])
+        gems_count[i[1]] = gems_count.get(i[1], 0) + 1
+
+    for customer in top_customers:
+        response.append({
+            'username': customer.login,
+            'spent_money': customer.spent_money,
+            'gems': [i for i in gems_lists[customer.login] if gems_count[i] > 1]
+        })
+
+    return Response(status=status.HTTP_200_OK, data={'response': response})
